9707e35e40a217d93273386614d59eed109e83fd
EC2 Default User <ec2-user@ip-172-31-9-251.ap-south-1.compute.internal>
2026-09-08 13:41:01 +0000

    Prepare Sprint 6 cloud infrastructure

--- backend/server.js
+++ backend/server.js
@@ -71,7 +71,7 @@ const upload = multer({
 });
 
 app.disable('x-powered-by');
-app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
+app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' }, hsts: false, contentSecurityPolicy: false }));
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
